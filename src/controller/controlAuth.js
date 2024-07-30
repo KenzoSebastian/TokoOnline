6c081a096ea9f8ea02aab54c22ced09d13cc4754
KenzoSebastian <kenzosebastian4@gmail.com>
2024-07-30 15:14:28 +0700

    UI Login

--- src/controller/controlAuth.js
+++ src/controller/controlAuth.js
@@ -1,6 +1,7 @@
 const stringRandom = require("../generator/stringGenerate");
 
 const login = (req, res) => {
+    const role = req.query.role;
     res.render("pages/login", {
         title: "Sign In",
         layout: "layouts/main"
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,39 @@
 module.exports = {
   content: ["./views/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true
+    },
+    screens: {
+      "sm": "544px",
+      "md": "896px",
+      "lg": "1024px",
+    },
+    extend: {
+      colors: {
+        primary: {
+          1: "#00FF00",
+          2: "#00e500",
+          3: "#00cc00",
+          4: "#00b200",
+          5: "#009900",
+          6: "#007f00",
+          7: "#006600",
+          8: "#004c00",
+          9: "#003300",
+          10: "#001900",
+          11: "#19FF19",
+          12: "#33FF33",
+          13: "#4DFF4D",
+          14: "#66FF66",
+          15: "#80FF80",
+          16: "#99FF99",
+        },
+      },
+      fontFamily: {
+        inter: "inter, sans-serif, Arial"
+      }
+    },
   },
   plugins: [],
 }
