cb5f584b8bfe35bc022771914a094ff4584dbc47
KenzoSebastian <kenzosebastian4@gmail.com>
2024-07-30 21:33:43 +0700

    UI register & reusable component

--- src/controller/controlAuth.js
+++ src/controller/controlAuth.js
@@ -4,6 +4,7 @@ const login = (req, res) => {
     const role = req.query.role;
     res.render("pages/login", {
         title: "Sign In",
+        greeting: "hi there! we're glad you're back again",
         layout: "layouts/main"
     });
 };
@@ -11,6 +12,7 @@ const login = (req, res) => {
 const register = (req, res) => {
     res.render("pages/register", {
         title: "Sign Up",
+        greeting: "hello, Fill out this form to join our amazing community!",
         layout: "layouts/main"
     });
 };
